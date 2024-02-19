82483e5c27e8a0517f6d00a6b799bbf75c2ddfc1
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2024-02-19 22:13:48 +0300

    added second tasks handler + htmx

--- main.go
+++ main.go
@@ -5,6 +5,28 @@ import (
 	"github.com/gofiber/template/html/v2"
 )
 
+type Task struct {
+	Title string
+	Done  bool
+}
+
+type TaskPageData struct {
+	PageTitle string
+	Tasks     []Task
+}
+
+func tasks(c *fiber.Ctx) error {
+	data := TaskPageData{
+		PageTitle: "Todays Tasks",
+		Tasks: []Task{
+			{Title: "Make tiktok video about SSR and Go", Done: false},
+			{Title: "Integrate golang SSR and Tailwinds CSS", Done: true},
+			{Title: "Carry whittington on fortnite", Done: true},
+		},
+	}
+	return c.Render("tasks", data)
+}
+
 func main() {
 
 	engine := html.New("./web/views", ".html")
@@ -21,10 +43,12 @@ func main() {
 		})
 	})
 
+	app.Get("/tasks", tasks)
+
 	app.Listen(":8080")
 }
 
 // TODO:
 // add Parcel
-// add templ
-// add htmx
+// add templ +
+// add htmx +
